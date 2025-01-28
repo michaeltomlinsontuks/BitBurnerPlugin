3002f8baeb1c9a15d55dbded1d2bc7db93b162f7
michaeltomlinsontuks <159887992+michaeltomlinsontuks@users.noreply.github.com>
2025-01-28 12:45:11 +0200

    Fixed WebSocketClient.kt

--- src/main/kotlin/WebSocketClient.kt
+++ src/main/kotlin/WebSocketClient.kt
@@ -1,9 +1,8 @@
 import io.ktor.client.*
-import io.ktor.client.features.websocket.*
-import io.ktor.client.features.websocket.WebSockets
+import io.ktor.client.plugins.websocket.*
 import io.ktor.client.request.*
 import io.ktor.http.*
-import io.ktor.util.*
+import io.ktor.websocket.*
 import kotlinx.coroutines.*
 
 suspend fun main() {
