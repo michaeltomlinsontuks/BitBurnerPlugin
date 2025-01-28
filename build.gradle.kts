d7cc544f58cea81e3b253c290f2443bc53f22564
michaeltomlinsontuks <159887992+michaeltomlinsontuks@users.noreply.github.com>
2025-01-28 22:11:22 +0200

    Update Gradle and plugin versions:
    - Upgrade Gradle to 8.5
    - Update IntelliJ plugin to 1.16.1 for compatibility

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     kotlin("jvm") version "1.9.20"
-    id("org.jetbrains.intellij") version "1.15.0"
+    id("org.jetbrains.intellij") version "1.16.1"
     kotlin("plugin.serialization") version "1.9.20"
 }
 
